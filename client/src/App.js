6b02f714a5c07f7150e96babfb401be7ad0bdd7a
Jean-Paul Zambrano-Leon <63323302+jpzambranoleon@users.noreply.github.com>
2023-05-29 23:40:34 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Chat from "./pages/chat/Chat";
 
 const mdTheme = createTheme();
-// main function 
+// main function to write on
 function App() {
   return (
     <div className="App">
